eb93c925b1894d38a020a90bbac489179fe912a6
Hauke Bartsch <HaukeBartsch@gmail.com>
2025-09-19 20:35:45 +0200

    change playback

--- js/gallery.js
+++ js/gallery.js
@@ -21,7 +21,7 @@ function keyFromCanvas(canvas) {
 
 function playback(canvas, structure) {
   if (structure.length < 1 || typeof structure[structure.length - 1].pos == "undefined" /*|| structure[structure.length - 1].pos.length < 2 */)
-    return; // do nothing
+    return; // do nothing 
   
   // what is the length of this structures display?
   var start = null;
